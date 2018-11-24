e0433a39bcf039e8a214ce245a7b82f093e521bc
Michael Donovan <mike@eggborne.com>
2018-11-24 13:18:09 -0800

    Add createMarkers function, tickHand color effect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -47,14 +47,17 @@ class App extends React.Component {
         }
       }
     };
+    this.createMarkers = this.createMarkers.bind(this);
     this.mainLoop = this.mainLoop.bind(this);
     this.handleTempoChange = this.handleTempoChange.bind(this);
     this.handlePressPlay = this.handlePressPlay.bind(this);
+
+    
     requestAnimationFrame(this.mainLoop);
   }
 
   componentDidMount() {
-
+    this.createMarkers(16);
   }
 
   componentWillUnmount() {
@@ -67,28 +70,80 @@ class App extends React.Component {
   }
 
   mainLoop() {
+    let time = Date.now();
     let step = Math.ceil(ticker / this.state.tempo);
-    let kickSound = this.state.sounds.kick.element;
-    let snareSound = this.state.sounds.snare.element;
+    let kickSound = document.getElementById('kick')
+    let snareSound = document.getElementById('snare')
+    let hatSound = document.getElementById('hat')
     let tickHand = document.getElementById('tick-hand');
-    // console.log('ang', tickHand.style.transform)
     if (this.state.playing) {
-      tickHand.style.backgroundColor = 'green';
+      // tickHand.style.backgroundColor = 'green';
       tickHand.style.transform = `rotate(${step}deg)`;
       if (ticker % 180 === 0) {
         // kickSound.play();
       }
+      if (step % 90 === 0) {
+        kickSound.play();
+        tickHand.style.backgroundColor = 'red';
+        setTimeout(() => {
+          tickHand.style.backgroundColor = 'gray';
+        },100)
+      }
+      if ((step+45) % 90 === 0) {
+        snareSound.play();
+        tickHand.style.backgroundColor = 'green';
+        setTimeout(() => {
+          tickHand.style.backgroundColor = 'gray';
+        },100)
+      }
+      
       ticker++;
+      if (ticker === 360) {
+        ticker = 0;
+      }
     } else {
       tickHand.style.backgroundColor = 'red';
     }
-
-    if (ticker === 360) {
-      ticker = 0;
-    }
-
+    
     requestAnimationFrame(this.mainLoop);
   }
+
+
+  createMarkers(num) {
+    let radius = Math.round(shorterDimension * 0.48);
+    let headerHeight = Math.round(shorterDimension * 0.15);
+    console.log('radius', radius)
+    for (let i = 0; i < (num/2); i++) {
+      let wedgeSize = (360 / num) * i;
+      console.log('wedge', wedgeSize)
+      let newMark = document.createElement('mark0');
+      let markHeight = radius * 2.1;
+      let markProtrusion = radius * 0.05;
+      if (i % 2 === 0) {
+        newMark.style.backgroundColor = '#222';
+      } else {
+        newMark.style.backgroundColor = '#444';
+      }
+      if (i % (num/4) === 0) {
+        newMark.style.width = '1vmin'
+      } else {
+        newMark.style.width = '0.5vmin';
+
+      }
+      newMark.style.height = `${markHeight}px`;
+      
+      newMark.style.position = 'absolute';
+      newMark.style.zIndex = 2;
+      newMark.style.transformOrigin = '50% 50%'
+      // newMark.style.transform = `translateY(${-radius*1.05}px)`;
+      newMark.style.transform = `rotate(${wedgeSize}deg)`;
+      newMark.style.top = `${document.getElementById('drum-circle').offsetTop+headerHeight-markProtrusion}px`;
+      document.getElementById('main').appendChild(newMark);
+    }
+
+  }
+
+
 
   handleTempoChange() {
     let bpm = event.target.value;
